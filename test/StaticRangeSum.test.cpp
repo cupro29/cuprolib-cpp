da60d5ed618c2cfbebef75ca1dcbdf666d77dd68
m.bando <cupro.m454@gmail.com>
2022-10-16 03:00:05 +0900

    test: DisjointSparseTable

---
+++ test/StaticRangeSum.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
+
+#include <bits/stdc++.h>
+
+#include "../include/DST"
+
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+  int N, Q;
+  std::cin >> N >> Q;
+  std::vector<long long> a(N);
+  for (auto& e : a) std::cin >> e;
+  DisjointSparseTabel<long long> dst(a, std::plus<long long>());
+  for (int i = 0; i < Q; i++) {
+    int l, r;
+    std::cin >> l >> r;
+    std::cout << dst.query(l, r) << "\n";
+  }
+}
